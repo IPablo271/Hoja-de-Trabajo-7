7633b6bdb2158f25e2d4cbad5515732984a4c112
IPablo271 <69815580+IPablo271@users.noreply.github.com>
2021-04-16 17:45:38 -0600

    Hoja de Trabjo 7
    Hoja de trabajo 7 Diccionario para Espanol, Frances e ingles.

---
+++ BST/main.java
@@ -0,0 +1,365 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.HashMap;
+public class main {
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<String> primeradivision=new ArrayList<String>();
+        ArrayList<String[]> segundadivision=new ArrayList<>();
+        Scanner scan2 = new Scanner(System.in);
+        ArrayList<HashMap> espanol=new ArrayList<>();
+        ArrayList<HashMap> ingles=new ArrayList<>();
+        ArrayList<HashMap> frances=new ArrayList<>();
+        ArrayList<String> palabras1=new ArrayList<>();
+        ArrayList<String> palabras2=new ArrayList<>();
+        ArrayList<String> palabras3=new ArrayList<>();
+        ArrayList<ArrayList> listaspalabras=new ArrayList<>();
+        ArrayList<ArrayList<HashMap>> listafinal=new ArrayList<>();
+        ArrayList<ArrayList<HashMap>> listafinal2=new ArrayList<>();
+        ArrayList<ArrayList<HashMap>> listafinal3=new ArrayList<>();
+        ArrayList<Association> ListaFinalset=new ArrayList<>();
+        ArrayList<Association> ListaFinalespanol=new ArrayList<>();
+        ArrayList<Association> ListaFinalfrances=new ArrayList<>();
+        ArrayList<Association> EspanolIngles=new ArrayList<>();
+        ArrayList<Association> EspanolFrances=new ArrayList<>();
+        ArrayList<Association> InglesEspanol=new ArrayList<>();
+        ArrayList<Association> Inglesfrances=new ArrayList<>();
+        ArrayList<Association> FrancesEspanol=new ArrayList<>();
+        ArrayList<Association> FrancesIngles=new ArrayList<>();
+        System.out.println("Bienvenido al programa");
+        //Stacks Creados con el sentido que almacenen las diferentes associciones de tal manera que se visualizar las diferentes traducciones
+        File file=new File("palabras.txt");
+        File file2=new File("oracion.txt");
+        Scanner scan3=new Scanner(file2);
+        Scanner scan=new Scanner(file);
+        String filecontent="";
+        while(scan.hasNext()){
+            filecontent=filecontent.concat(scan.nextLine()+"\n");
+        }
+        String[] arr=filecontent.split("\n");
+        for(int i=0;i<arr.length;i++){
+            String a=arr[i];
+            primeradivision.add(a);
+        }
+        for(int c=0;c<primeradivision.size();c++){
+            String a=primeradivision.get(c);
+            String[] arr2=a.split(",");
+            segundadivision.add(arr2);
+        }
+        for(int i=0;i<segundadivision.size();i++){
+            String sublista[]=segundadivision.get(i);
+            String uno=sublista[0];
+            String dos=sublista[1];
+            String tres=sublista[2];
+            palabras1.add(uno);
+            palabras2.add(dos);
+            palabras3.add(tres);
+            HashMap<String,String> mapa1=new HashMap<>();
+            HashMap<String,String> mapa12=new HashMap<>();
+
+            Association b=new Association(uno,dos);
+            Association bc=new Association(uno,tres);
+            Association espanoli=new Association(dos,uno);
+            Association espanolf=new Association(dos,tres);
+            Association franceespanol=new Association(tres,uno);
+            Association franceesingles=new Association(tres,dos);
+
+
+            mapa1.put(uno,dos);
+            mapa12.put(uno,tres);
+            ingles.add(mapa1);
+            ingles.add(mapa12);
+            HashMap<String,String> mapa2=new HashMap<>();
+            HashMap<String,String> mapa22=new HashMap<>();
+            mapa2.put(dos,uno);
+            mapa22.put(dos,tres);
+            espanol.add(mapa2);
+            espanol.add(mapa22);
+            HashMap<String,String> mapa3=new HashMap<>();
+            HashMap<String,String> mapa32=new HashMap<>();
+            mapa3.put(tres,uno);
+            mapa32.put(tres,uno);
+            frances.add(mapa3);
+            frances.add(mapa32);
+            InglesEspanol.add(b);
+            Inglesfrances.add(bc);
+            EspanolIngles.add(espanoli);
+            EspanolFrances.add(espanolf);
+            FrancesEspanol.add(franceespanol);
+            FrancesIngles.add(franceesingles);
+
+        }
+        int cantidadtp=palabras1.size()+palabras2.size()+palabras3.size();
+        int v=0;
+        for(int i=0;i<ingles.size();i=i+2){
+           HashMap c =ingles.get(i);
+           HashMap d=ingles.get(i+1);
+           ArrayList<HashMap> listainterna=new ArrayList<>();
+           listainterna.add(c);
+           listainterna.add(d);
+           listafinal.add(listainterna);
+
+        }
+        for(int i=0;i<espanol.size();i=i+2){
+            HashMap c =espanol.get(i);
+            HashMap d=espanol.get(i+1);
+            ArrayList<HashMap> listainterna=new ArrayList<>();
+            listainterna.add(c);
+            listainterna.add(d);
+            listafinal2.add(listainterna);
+
+        }
+        for(int i=0;i<frances.size();i=i+2){
+            HashMap c =frances.get(i);
+            HashMap d=frances.get(i+1);
+            ArrayList<HashMap> listainterna=new ArrayList<>();
+            listainterna.add(c);
+            listainterna.add(d);
+            listafinal3.add(listainterna);
+        }
+        for(int i=0;i<palabras1.size();i++){
+            String a=palabras1.get(i);
+            ArrayList<HashMap> maj=listafinal.get(i);
+            Association b=new Association(a,maj);
+            ListaFinalset.add(b);
+        }
+        for(int i=0;i<palabras2.size();i++){
+            String a=palabras2.get(i);
+            ArrayList<HashMap> maj=listafinal2.get(i);
+            Association b=new Association(a,maj);
+            ListaFinalespanol.add(b);
+        }
+        for(int i=0;i<palabras3.size();i++){
+            String a=palabras3.get(i);
+            ArrayList<HashMap> maj=listafinal3.get(i);
+            Association b=new Association(a,maj);
+            ListaFinalfrances.add(b);
+        }
+
+
+        BinaryTree<Association> arbolIngles=new BinaryTree<Association>(ListaFinalset.get(0));
+        BinaryTree<Association> arbolespanol=new BinaryTree<Association>(ListaFinalespanol.get(0));
+        BinaryTree<Association> arbolfrances=new BinaryTree<Association>(ListaFinalfrances.get(0));
+
+        for (int n = 0; n < ListaFinalset.size(); n++) {
+            if ((n+1) < ListaFinalset.size()) {
+                BinaryTree<Association> btSiguiente = new BinaryTree<>(ListaFinalset.get(n+1));
+                BinaryTree.agregar(arbolIngles, btSiguiente);
+            }
+        }
+        System.out.print("\n Este es el arbol binario in order: "+arbolIngles.inOrder(arbolIngles));
+        String contenido="";
+        while(scan3.hasNext()){
+            contenido=contenido.concat(scan3.nextLine()+"\n");
+        }
+        System.out.print(contenido);
+        ArrayList<String> subdivision=new ArrayList<>();
+        String[] arr2=contenido.split(" ");
+        for(int i=0;i<arr2.length;i++){
+            String a=arr2[i];
+            String b=a.toLowerCase();
+            subdivision.add(b);
+        }
+        boolean rungeneral=true;
+
+        while(rungeneral){
+            System.out.print("\nBienvenido al programa");
+            System.out.print("\nIngrese la opcion que desee");
+            System.out.print("\nEn que idioma esta el archivo que desea traducir");
+            System.out.print("\n1.Espanol");
+            System.out.print("\n2.Ingles");
+            System.out.print("\n3.Frances");
+            System.out.print("\n4.Terminar el programa");
+            System.out.print("\nSeleccione su opcion: ");
+            int a=scan2.nextInt();
+            if(a==1){
+                boolean subrun=true;
+                while(subrun){
+                    System.out.print("\nA que idioma quiere traducir su archivo");
+                    System.out.print("\n1. Ingles");
+                    System.out.print("\n2. Frances");
+                    System.out.print("\nIngrese su opcion: ");
+                    int h=scan2.nextInt();
+                    if(h==1){
+                        System.out.print("Traduccion espanol ingles");
+                        BinaryTree<Association> Arbol1=new BinaryTree<Association>(EspanolIngles.get(0));
+                        for (int n = 0; n < EspanolIngles.size(); n++) {
+                            if ((n+1) < EspanolIngles.size()) {
+                                BinaryTree<Association> btSiguiente = new BinaryTree<>(EspanolIngles.get(n+1));
+                                BinaryTree.agregar(Arbol1,btSiguiente);
+                            }
+                        }
+                        String concatenado="";
+                        for( int i=0;i<subdivision.size();i++){
+                            String b=subdivision.get(i);
+                            String palabra=Arbol1.buscar(b);
+                            if(palabra==null){
+                                concatenado=concatenado+"* "+b+" *";
+                            }
+                            else{
+                                concatenado=concatenado+" * "+palabra+" * ";
+                            }
+                        }
+                        System.out.print("\nLa oracion traducida es: "+concatenado);
+                        subrun=false;
+                    }
+                    if(h==2){
+                        System.out.print("Traduccion espanol frances");
+                        BinaryTree<Association> Arbol1=new BinaryTree<Association>(EspanolFrances.get(0));
+                        for (int n = 0; n < EspanolFrances.size(); n++) {
+                            if ((n+1) < EspanolFrances.size()) {
+                                BinaryTree<Association> btSiguiente = new BinaryTree<>(EspanolFrances.get(n+1));
+                                BinaryTree.agregar(Arbol1,btSiguiente);
+                            }
+                        }
+                        String concatenado="";
+                        for( int i=0;i<subdivision.size();i++){
+                            String b=subdivision.get(i);
+                            String palabra=Arbol1.buscar(b);
+                            if(palabra==null){
+                                concatenado=concatenado+"* "+b+" *";
+                            }
+                            else{
+                                concatenado=concatenado+" * "+palabra+" * ";
+                            }
+                        }
+                        System.out.print("\nLa oracion traducida es: "+concatenado);
+                        subrun=false;
+                    }
+                    else{
+                        System.out.print("Ingrese una opcion valida");
+                    }
+                }
+
+            }
+            if(a==2){
+                boolean subrun=true;
+                while(subrun){
+                    System.out.print("\nA que idioma quiere traducir su archivo");
+                    System.out.print("\n1. Espanol");
+                    System.out.print("\n2. Frances");
+                    System.out.print("\nIngrese su opcion: ");
+                    int h=scan2.nextInt();
+                    if(h==1){
+                        System.out.print("Traduccion ingles espanol");
+                        BinaryTree<Association> Arbol1=new BinaryTree<Association>(InglesEspanol.get(0));
+                        for (int n = 0; n < InglesEspanol.size(); n++) {
+                            if ((n+1) < InglesEspanol.size()) {
+                                BinaryTree<Association> btSiguiente = new BinaryTree<>(InglesEspanol.get(n+1));
+                                BinaryTree.agregar(Arbol1,btSiguiente);
+                            }
+                        }
+                        String concatenado="";
+                        for( int i=0;i<subdivision.size();i++){
+                            String b=subdivision.get(i);
+                            String palabra=Arbol1.buscar(b);
+                            if(palabra==null){
+                                concatenado=concatenado+"* "+b+" *";
+                            }
+                            else{
+                                concatenado=concatenado+" * "+palabra+" * ";
+                            }
+                        }
+                        System.out.print("\nLa oracion traducida es: "+concatenado);
+                        subrun=false;
+                    }
+                    if(h==2){
+                        BinaryTree<Association> Arbol1=new BinaryTree<Association>(Inglesfrances.get(0));
+                        for (int n = 0; n < Inglesfrances.size(); n++) {
+                            if ((n+1) < Inglesfrances.size()) {
+                                BinaryTree<Association> btSiguiente = new BinaryTree<>(Inglesfrances.get(n+1));
+                                BinaryTree.agregar(Arbol1,btSiguiente);
+                            }
+                        }
+                        String concatenado="";
+                        for( int i=0;i<subdivision.size();i++){
+                            String b=subdivision.get(i);
+                            String palabra=Arbol1.buscar(b);
+                            if(palabra==null){
+                                concatenado=concatenado+"* "+b+" *";
+                            }
+                            else{
+                                concatenado=concatenado+" * "+palabra+" * ";
+                            }
+                        }
+                        System.out.print("\nLa oracion traducida es: "+concatenado);
+                        subrun=false;
+                    }
+                    else{
+                        System.out.print("Ingrese una opcion valida");
+                    }
+                }
+
+            }
+            if(a==3){
+                boolean subrun=true;
+                while(subrun){
+                    System.out.print("\nA que idioma quiere traducir su archivo");
+                    System.out.print("\n1. Espanol");
+                    System.out.print("\n2. Ingles");
+                    System.out.print("\nIngrese su opcion: ");
+                    int h=scan2.nextInt();
+                    if(h==1){
+                        System.out.print("Traduccion frances espanol");
+                        BinaryTree<Association> Arbol1=new BinaryTree<Association>(FrancesEspanol.get(0));
+                        for (int n = 0; n < FrancesEspanol.size(); n++) {
+                            if ((n+1) < FrancesEspanol.size()) {
+                                BinaryTree<Association> btSiguiente = new BinaryTree<>(FrancesEspanol.get(n+1));
+                                BinaryTree.agregar(Arbol1,btSiguiente);
+                            }
+                        }
+                        String concatenado="";
+                        for( int i=0;i<subdivision.size();i++){
+                            String b=subdivision.get(i);
+                            String palabra=Arbol1.buscar(b);
+                            if(palabra==null){
+                                concatenado=concatenado+"* "+b+" *";
+                            }
+                            else{
+                                concatenado=concatenado+" * "+palabra+" * ";
+                            }
+                        }
+                        System.out.print("\nLa oracion traducida es: "+concatenado);
+                        subrun=false;
+                    }
+                    if(h==2){
+                        System.out.print("Traduccion frances ingles");
+                        BinaryTree<Association> Arbol1=new BinaryTree<Association>(FrancesIngles.get(0));
+                        for (int n = 0; n < FrancesIngles.size(); n++) {
+                            if ((n+1) < FrancesIngles.size()) {
+                                BinaryTree<Association> btSiguiente = new BinaryTree<>(FrancesIngles.get(n+1));
+                                BinaryTree.agregar(Arbol1,btSiguiente);
+                            }
+                        }
+                        String concatenado="";
+                        for( int i=0;i<subdivision.size();i++){
+                            String b=subdivision.get(i);
+                            String palabra=Arbol1.buscar(b);
+                            if(palabra==null){
+                                concatenado=concatenado+"* "+b+" *";
+                            }
+                            else{
+                                concatenado=concatenado+" * "+palabra+" * ";
+                            }
+                        }
+                        System.out.print("\nLa oracion traducida es: "+concatenado);
+                        subrun=false;
+                    }
+                    else{
+                        System.out.print("Ingrese una opcion valida");
+                    }
+                }
+
+            }
+            if(a==4){
+                System.out.print("Saliendo del programa");
+                break;
+            }
+            else{
+                System.out.print("Ingrese una opcion valida");
+            }
+        }
+
+    }
+}
